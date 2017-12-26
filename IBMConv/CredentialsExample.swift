c1caba2f9f58f933ed33824c6790b2effa44c4a6
Maxime Golfier <maxgolfier@gmail.com>
2017-12-26 11:47:24 +0100

    ii

---
+++ IBMConv/CredentialsExample.swift
@@ -0,0 +1,14 @@
+//
+//  CreditentialsExample.swift
+//  IBMConv
+//
+//  Created by GOLFIER on 26/12/2017.
+//  Copyright © 2017 GOLFIER. All rights reserved.
+//
+
+public struct CredentialsExample {
+    public static let CloudantUrl = "xxx"
+    public static let CloudantUsername = "xxx"
+    public static let CloudantPassword = "xxx"
+    public static let CloudantName = "xxx"
+}
--- IBMConv/ViewController/RegisterViewController.swift
+++ IBMConv/ViewController/RegisterViewController.swift
@@ -31,6 +31,11 @@ class RegisterViewController: UIViewController {
         let userPassword = passwordTextField.text;
         let userRepeatPassword = repeatTextField.text;
         
+        // Create a CouchDBClient
+        let cloudantURL = NSURL(string:Credentials.CloudantUrl)!
+        let client = CouchDBClient(url:cloudantURL as URL, username:Credentials.CloudantUsername, password:Credentials.CloudantPassword)
+        let dbName = Credentials.CloudantName
+        
         // Check for empty fields
         if((userEmail?.isEmpty)! || (userPassword?.isEmpty)! || (userRepeatPassword?.isEmpty)!){
             // Display alert message
@@ -45,20 +50,25 @@ class RegisterViewController: UIViewController {
             return;
         }
         
+        // Create a document
+        let create = PutDocumentOperation(id: "doc1", body: ["hello":"world"], databaseName: dbName) {(response, httpInfo, error) in
+            if let error = error {
+                print("Encountered an error while creating a document. Error:\(error)")
+            } else {
+                print("Created document \(response?["id"]) with revision id \(response?["rev"])")
+            }
+        }
+        client.add(operation:create)
+        
         //displayMyAlertMessage(userMessage: "coucou")
         return
     }
-    func displayMyAlertMessage(userMessage:String)
-    {
-        
+    
+    func displayMyAlertMessage(userMessage:String){
         let myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle: UIAlertControllerStyle.alert);
-        
         let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.default, handler:nil);
-        
         myAlert.addAction(okAction);
-        
         self.present(myAlert, animated:true, completion:nil);
-        
     }
     
     
